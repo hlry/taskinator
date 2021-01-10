3badac942a4d99247cebeb8c3d20071dc9dc5142
Hilary Richardson <hilary.richardson@gmail.com>
2021-01-09 22:02:06 -0800

    create drag and drop functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,6 +50,9 @@ var createTaskEl = function (taskDataObj) {
   // add task id as a custom attribute
   listItemEl.setAttribute("data-task-id", taskIdCounter);
 
+  //add draggable attribute
+  listItemEl.setAttribute("draggable", "true");
+
   // create div to hold task info and add to list item
   var taskInfoEl = document.createElement("div");
   // give it a class name
@@ -186,8 +189,49 @@ var taskStatusChangeHandler = function (event) {
   }
 };
 
+var dragTaskHandler = function (event) {
+  var taskId = event.target.getAttribute("data-task-id");
+  event.dataTransfer.setData("text/plain", taskId);
+  var getId = event.dataTransfer.getData("text/plain");
+  console.log("getId:", getId, typeof getId);
+}
+
+var dropZoneDragHandler = function (event) {
+  var taskListEl = event.target.closest(".task-list");
+  if (taskListEl) {
+    event.preventDefault();
+    console.dir(taskListEl);
+  }
+};
+
+var dropTaskHandler = function (event) {
+  var id = event.dataTransfer.getData("text/plain");
+  var draggableElement = document.querySelector("[data-task-id='" + id + "']");
+  var dropZoneEl = event.target.closest(".task-list");
+  var statusType = dropZoneEl.id;
+  // set status of task based on dropZone id
+  var statusSelectEl = draggableElement.querySelector("select[name='status-change']");
+  if (statusType === "tasks-to-do") {
+    statusSelectEl.selectedIndex = 0;
+  }
+  else if (statusType === "tasks-in-progress") {
+    statusSelectEl.selectedIndex = 1;
+  }
+  else if (statusType === "tasks-completed") {
+    statusSelectEl.selectedIndex = 2;
+  }
+  dropZoneEl.appendChild(draggableElement);
+};
+
+
+
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
+
+// for draggable functions
+pageContentEl.addEventListener("dragstart", dragTaskHandler);
+pageContentEl.addEventListener("dragover", dropZoneDragHandler);
+pageContentEl.addEventListener("drop", dropTaskHandler);
 
 // for edit and delete buttons
 pageContentEl.addEventListener("click", taskButtonHandler);
